ffe77836661b428f3caaf85929d11c05de88e61f
TenPassion <142400798+TenPassion@users.noreply.github.com>
2023-09-25 05:24:27 +0200

    kindroid.user.js

--- kindroid.user.js
+++ kindroid.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Kindroid Mod
+// @name         Kindroid Script
 // @version      1.0
 // @description  Modify background, sound notification, chat opacity, user and kin name on top.
 // @author       TemPassion
